390628af9d449816d48f7011710911907ca6c6d5
Ramanath1718 <ramanathbhat@64gmail.com>
2025-03-10 11:58:08 +0530

    done

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = "https://shop-backend-1s0o.onrender.com";
 
 export async function login(credentials) {
   const response = await fetch(`${API_URL}/login`, {
